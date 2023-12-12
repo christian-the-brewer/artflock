f8800b75f6f1f9e542a22340d5ae3d2533f2d8ef
Christian <brewer.christiang@gmail.com>
2023-12-11 20:41:55 -0500

    creates navbar

---
+++ src/config/Index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "Illustrations",
+        value: "illustrations" as const,
+        featured: [
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: "",
+            },
+            {
+                name: "New Additions",
+                href: "#",
+                imageSrc: "",
+            },
+            {
+                name: "Best Sellers",
+                href: "#",
+                imageSrc: "",
+            },
+        ]
+    },
+    {
+        label: "Photos",
+        value: "photos" as const,
+        featured: [
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: "",
+            },
+            {
+                name: "New Additions",
+                href: "#",
+                imageSrc: "",
+            },
+            {
+                name: "Best Sellers",
+                href: "#",
+                imageSrc: "",
+            },
+        ]
+    }
+]
