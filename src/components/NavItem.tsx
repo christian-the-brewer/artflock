3d4585f25145acdad20b9aabe4590f440b84c02a
christian-the-brewer <brewer.christiang@gmail.com>
2023-12-11 21:49:03 -0500

    creating navitems

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import {Button} from "@/components/ui/button";
+import {PRODUCT_CATEGORIES} from "@/config/Index";
+import {ChevronDown} from "lucide-react";
+import {cn} from "@/lib/utils";
+
+type Category = typeof PRODUCT_CATEGORIES[number]
+interface NavItemProps {
+    category: Category
+    handleOpen: () => void
+    isOpen: boolean
+    isAnyOpen: boolean
+}
+
+const NavItem = ({category, handleOpen, isOpen, isAnyOpen}: NavItemProps) => {
+    return <div className="flex">
+    <div className="relative flex items-center">
+        <Button className="gap-1.5" onClick={handleOpen} variant={isOpen ? "secondary" : "ghost"}>
+            {category.label}
+            <ChevronDown className={cn("h-4 w-4 transition-all text-muted-foreground",{
+                "-rotate-180": isOpen,
+            })} />
+        </Button>
+    </div>
+        </div>
+}
+export default NavItem
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -2,11 +2,36 @@
 
 import {number} from "prop-types";
 import {useState} from "react";
+import {PRODUCT_CATEGORIES} from "@/config/Index";
+import NavItem from "@/components/NavItem";
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null)
+
+    const isAnyOpen = activeIndex != null
+
     return (
-        <div className="flex gap-4 h-full"></div>
+        <div className="flex gap-4 h-full">
+            {PRODUCT_CATEGORIES.map((category, i) => {
+                const handleOpen = () => {
+                    if (activeIndex === i) {
+                        setActiveIndex(null);
+                    } else {
+                        setActiveIndex(i)
+                    }
+                }
+                const isOpen = i === activeIndex
+                return (
+                    <NavItem
+                    category={category}
+                    handleOpen={handleOpen}
+                    isOpen={isOpen}
+                    key={category.value}
+                    isAnyOpen={isAnyOpen}
+                    />
+                )
+            })}
+        </div>
     )
 }
 
