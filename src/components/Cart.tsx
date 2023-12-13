87decf6dde2b1adb8536372a5dccc81eced5dcc8
Christian <brewer.christiang@gmail.com>
2023-12-13 11:49:42 -0500

    setting up server

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -21,19 +21,19 @@ const Cart = () => {
 
     const fee = 1
 
-    const itemCount = 0
+    const itemCount = 1
     return <Sheet>
         <SheetTrigger className="group -m-2 flex items-center p-2 ">
             <ShoppingCart className="h-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
                           aria-hidden="true"
             />
             <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                0
+                1
             </span>
         </SheetTrigger>
         <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
             <SheetHeader className="space-y-2.5 pr-6">
-                <SheetTitle>Cart (0)</SheetTitle>
+                <SheetTitle>Cart (1)</SheetTitle>
             </SheetHeader>
             {itemCount > 0 ? (
                 <>
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,30 @@
 import express from "express"
+import {getPayloadClient} from "@/get_payload";
+import {nextApp, nextHandler} from "@/next_utils";
+import {res} from "pino-std-serializers";
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
 
-const start =  async () => {
-    const payload = await getPayloadClient()
+const start = async () => {
+    const payload = await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) => {
+                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+            },
+        },
+    })
+
+    app.use((req, res) => nextHandler(req, res))
+    nextApp.prepare().then(() => {
+        payload.logger.info("Next.js started")
+        app.listen(PORT, async () => {
+            payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+        })
+    })
+
 }
+
 
 start()
