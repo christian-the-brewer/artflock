9a1fac1a86be60f2f8be71fe61ea86a0a53e6710
Christian <brewer.christiang@gmail.com>
2023-12-14 14:43:24 -0500

    creating api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from "express"
-
 import {nextApp, nextHandler} from "./next_utils";
 import {res} from "pino-std-serializers";
 import {getPayloadClient} from "./get_payload";
+import * as trpcExpress from "@trpc/server/adapters/express"
+import {appRouter} from "@/trpc";
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
+
+const createContext = ({req, res}: trpcExpress.CreateExpressContextOptions) => ({
+    req,
+    res,
+})
 
 const start = async () => {
     const payload = await getPayloadClient({
@@ -16,6 +22,11 @@ const start = async () => {
             },
         },
     })
+
+    app.use("/api/trpc", trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    }))
 
     app.use((req, res) => nextHandler(req, res))
     nextApp.prepare().then(() => {
